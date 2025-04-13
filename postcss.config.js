a067611c0bbd019c2af0c533170e3cfba1aff9c0
Florian Jourde <jourdeflorian@gmail.com>
2025-04-13 20:25:32 +0200

    feat(glow): begin glow effect implementation

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    'tailwindcss': {},
+    'autoprefixer': {},
+    'postcss-preset-env': {
+      stage: 1,
+      features: {
+        'custom-properties': true
+      }
+    }
   },
 }
