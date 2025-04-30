dc710d24355dfcd132bbf7a44623afda66918df5
Florian Jourde <jourdeflorian@gmail.com>
2025-04-30 04:16:51 +0200

    fix(auth): adjust reset password email template

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -162,7 +162,7 @@ class ResetPasswordController extends AbstractController
         }
 
         $email = (new TemplatedEmail())
-            ->from(new Address('contact@quizstack.io', 'Reset Password'))
+            ->from(new Address('contact@quizstack.io', 'Quizstack'))
             ->to((string)$user->getEmail())
             ->subject('Your password reset request')
             ->htmlTemplate('reset_password/email.html.twig')
